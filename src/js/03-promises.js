02da57e835d0e40a2fc9b24460f53f03a5797fdd
NataliaMalynovska <natalimalinovska57@gmail.com>
2022-06-22 00:17:07 +0300

    fixing

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const submit = document.querySelector("button");
 const form = document.querySelector(".form");
 
 form.addEventListener("submit", e => {
@@ -10,29 +9,24 @@ form.addEventListener("submit", e => {
     const step= formElements.step.valueAsNumber ;
     const position= formElements.amount.valueAsNumber;
 
-    for (let i = 0; i < position; i += 1) {
-        delay += step;
-
-      createPromise(i, delay )
+    for (let i = 1; i <= position; i += 1) {
+    createPromise(i, delay )
         .then(({ position, delay }) => {
-          console.log(position);
     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`); 
 })
         .catch(({ position, delay }) => {
     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`); 
 });
+    delay += step;
     }
-}
+}   
 );
 
 function createPromise(position, delay) {
   return new Promise ((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3; 
- 
   setTimeout(() => {
     if (shouldResolve) {
-      console.log(position);
-      console.log(delay);
   resolve ({ position, delay });
   } 
       reject ({ position, delay });
